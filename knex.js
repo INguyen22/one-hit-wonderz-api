1f3a0dab2786be5bc7c4858ce8f21c75f0a4bde9
Abby Luce <abby.c.luce@gmail.com>
2022-09-18 13:00:27 -0600

    Add env to knex config

--- knex.js
+++ knex.js
@@ -1,5 +1,6 @@
 const knex = require('knex')
 const config = require('./knexfile')
+require("dotenv").config()
 
 const environment = process.env.NODE_ENV || 'development'
 const knexConfig = config[environment];
