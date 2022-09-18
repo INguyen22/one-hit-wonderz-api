aeb66203168b740647b5f79878e9d996622d2c16
Abby Luce <abby.c.luce@gmail.com>
2022-09-18 12:37:26 -0600

    Revert "make it better"
    This reverts commit 10b4b2e05bfcfed4a1c152dcaa43234addf4a607.

--- knex.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex')
-const config = require('./knexfile')
-
-const environment = process.env.NODE_ENV || 'development'
-const knexConfig = config[environment];
-
-module.exports = knex(knexConfig);
